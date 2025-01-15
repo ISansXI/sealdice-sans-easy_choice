3f7d87a8c98020ef5618f880016af77af3d4998e
sans <1654248556@qq.com>
2025-01-15 19:22:07 +0800

    更新

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import { sample } from "lodash-es";
-import { nameList } from "./utils";
+function generateRandomInteger(x, y) {
+  return Math.floor(Math.random() * (y - x + 1)) + x;
+}
 
 function main() {
   // 注册扩展
-  let ext = seal.ext.find('test');
+  let ext = seal.ext.find('sans-ez-choice');
   if (!ext) {
-    ext = seal.ext.new('test', '木落', '1.0.0');
+    ext = seal.ext.new('sans-ez-choice', '地星 AKA Sans', '1.0.0');
     seal.ext.register(ext);
   }
 
   // 编写指令
   const cmdSeal = seal.ext.newCmdItemInfo();
-  cmdSeal.name = 'seal';
-  cmdSeal.help = '召唤一只海豹，可用.seal <名字> 命名';
+  cmdSeal.name = 'sans-ez-choice';
+  cmdSeal.help = '随机选择你想要的东西，使用.s指令使用，比如：\n.s 左边 右边 上边 下边';
 
   cmdSeal.solve = (ctx, msg, cmdArgs) => {
     let val = cmdArgs.getArgN(1);
@@ -23,16 +24,19 @@ function main() {
         return ret;
       }
       default: {
-        // 命令为 .seal XXXX，取第一个参数为名字
-        if (!val) val = sample(nameList); // 无参数，随机名字
-        seal.replyToSender(ctx, msg, `你抓到一只海豹！取名为${val}\n它的逃跑意愿为${Math.ceil(Math.random() * 100)}`);
-        return seal.ext.newCmdExecuteResult(true);
+        let args = String(cmdArgs.getRestArgsFrom(1));
+        let argsArr = args.split(" ");
+        let sum = argsArr.length;
+        let index = generateRandomInteger(0, sum - 1);
+        seal.replyToSender(ctx, msg, `"${argsArr[index]}"`);
+        const ret = seal.ext.newCmdExecuteResult(true);
+        return ret;
       }
     }
   }
 
   // 注册命令
-  ext.cmdMap['seal'] = cmdSeal;
+  ext.cmdMap['s'] = cmdSeal;
 }
 
 main();
